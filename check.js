cc8e397a14c99fd0b8e61d96c63845bf0f76ae96
kklai <kkrebeccalai@gmail.com>
2017-06-14 17:05:12 -0400

    parsed files

---
+++ check.js
@@ -0,0 +1,38 @@
+var { _, cheerio, d3, fs, glob, io, queue, request } = require('scrape-stl');
+
+var listofdays = ["05-16", "05-17", "05-18", "05-19", "05-22", "05-23", "05-24", "05-25", "05-26", "05-29", "06-01", "06-02", "06-08", "06-09", "06-12", "06-13"];
+var stats = [];
+_.each(listofdays, function(date){
+
+  var file = fs.readFileSync("stop-times-2017-" + date + ".json");
+  file = JSON.parse(file);
+  var trips = [];
+  _.each(Object.keys(file), function(f){
+    var trip_id = f.split(" ")[0];
+    if (trips.indexOf(trip_id) == -1) {
+      trips.push(f.split(" ")[0]);
+    }
+  });
+  console.log(trips)
+  stats.push({
+    date: date,
+    trains: trips.length
+  });
+
+
+  function n(n){
+      return n > 9 ? "" + n: "0" + n;
+  }
+
+  function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = n(hour) + ':' + n(min) + ':' + n(sec) ;
+    return time;
+  }
+
+
+});
+fs.writeFileSync("stats.json", JSON.stringify(stats, null, 2));
--- parse.js
+++ parse.js
@@ -1,39 +1,63 @@
-var GtfsRealtimeBindings = require('gtfs-realtime-bindings')
-var { _, cheerio, d3, fs, glob, io, queue, request } = require('scrape-stl')
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var { _, cheerio, d3, fs, glob, io, queue, request } = require('scrape-stl');
 
-var tripStop2time = {}
-glob.sync(__dirname + '/2017-06-05/*').forEach(path => {
-  var slug = path.split('/').slice(-1)[0]
+var listofdays = ["05-16", "05-17", "05-18", "05-19", "05-22", "05-23", "05-24", "05-25", "05-26", "05-29", "06-01", "06-02", "06-08", "06-09", "06-12", "06-13"];
 
-  try {
-    if (slug == 'gtfs-2017-06-05-00-06-05') return
-    var buffer = fs.readFileSync(path)
-    var feed = GtfsRealtimeBindings.FeedMessage.decode(buffer)
-  } catch (e) {
-    return
+
+var stats = {};
+_.each(listofdays, function(date){
+
+  var tripStop2time = {};
+  glob.sync(__dirname + '/../subway/2017-' + date + '/*').forEach(path => {
+    var slug = path.split('/').slice(-1)[0];
+    console.log(path)
+
+    try {
+      var buffer = fs.readFileSync(path);
+      var feed = GtfsRealtimeBindings.FeedMessage.decode(buffer);
+    } catch (e) {
+      return;
+    }
+
+    feed.entity.forEach(entity => {
+      if (!entity.trip_update) return;
+
+      var trip = entity.trip_update.trip.trip_id.split("..")[0];
+      var route = trip.split("_")[1].substring(0,1);
+      if (route == "4" || route == "5" || route == "6") {
+        entity.trip_update.stop_time_update.forEach(d => {
+          var stop = d.stop_id;
+          var time = (d.departure || d.arrival).time.low;
+          tripStop2time[trip + ' ' + stop] = time;
+        });
+      }
+    });
+  });
+
+  var bytrips = _.groupBy(tripStop2time);
+  if (!stats[date]) {
+    stats[date] = [];
   }
+  _.each(bytrips, function(trip, trip_id){
+    stats[date].push({
+      trip_id: trip_id,
+      stops: trip.length
+    });
+  });
 
-  feed.entity.forEach(entity => {
-    if (!entity.trip_update) return
+  io.writeDataSync(__dirname + '/stop-times-2017-' + date + '.json', tripStop2time);
 
-    var trip = entity.trip_update.trip.trip_id
-    entity.trip_update.stop_time_update.forEach(d => {
-      var stop = d.stop_id
-      var time = (d.departure || d.arrival).time.low
-      tripStop2time[trip + ' ' + stop] = time
-    })
-  })
+  var tidy = d3.entries(tripStop2time).map(d => {
+    return {
+      route: d.key.split(" ")[0].split("_")[1].substring(0,1),
+      trip: d.key.split(' ')[0],
+      stop: d.key.split(' ')[1],
+      time: d.value
+    };
+  });
+  io.writeDataSync(__dirname + '/stop-times-2017-' + date + '.tsv', tidy);
+  
+});
 
-  console.log(slug)
-})
 
-io.writeDataSync(__dirname + '/tripStop2time.json', tripStop2time)
-
-var tidy = d3.entries(tripStop2time).map(d => {
-  return {
-    trip: d.key.split(' ')[0],
-    stop: d.key.split(' ')[1],
-    time: d.value
-  }
-})
-io.writeDataSync(__dirname + '/stop-times.tsv', tidy)
+fs.writeFileSync("stats.json", JSON.stringify(stats, null, 2));
